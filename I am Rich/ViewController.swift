81dbbd9a1a333c71ae9d13b0f3fd01887b96d2c2
Daria Zakharova <daria.zakharova.work@gmail.com>
2025-01-09 13:25:06 +0100

    Add project files

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Daria Zakharova on 17.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
